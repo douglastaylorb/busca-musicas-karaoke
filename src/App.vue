ef044c6cee5ce30c4f7533710ba38dcc754b09b1
douglastaylorb <douglastaylor20@gmail.com>
2024-11-09 09:57:11 -0300

    Adiciona metatags

--- src/App.vue
+++ src/App.vue
@@ -12,35 +12,6 @@ export default {
   components: {
     MusicSearch
   },
-  metaInfo: {
-    title: 'Encontre a Música',
-    meta: [
-      {
-        name: 'description',
-        content: 'Encontre a música que você deseja cantar'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1.0'
-      },
-      {
-        property: 'og:title',
-        content: 'Encontre a Música',
-      },
-      {
-        property: 'og:description',
-        content:  'Encontre a música que você deseja cantar',
-      },
-      {
-        property: 'og:image',
-        content: 'https://img.freepik.com/vetores-gratis/fundo-gradiente-de-karaoke_23-2150970025.jpg?semt=ais_hybrid',
-      },
-      {
-        property: 'og:url',
-        content: 'https://busca-musicas-karaoke.vercel.app/',
-      }
-    ]
-  }
 };
 </script>
 
