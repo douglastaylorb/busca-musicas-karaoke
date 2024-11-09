b2f224c1451dc8050832c4c39ea2b2ef51284885
douglastaylorb <douglastaylor20@gmail.com>
2024-11-09 09:50:02 -0300

    Adiciona metatags

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <main>
     <MusicSearch />
-
   </main>
 </template>
 
@@ -12,11 +11,38 @@ export default {
   name: 'App',
   components: {
     MusicSearch
+  },
+  metaInfo: {
+    title: 'Encontre a Música',
+    meta: [
+      {
+        name: 'description',
+        content: 'Encontre a música que você deseja cantar'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0'
+      },
+      {
+        property: 'og:title',
+        content: 'Encontre a Música',
+      },
+      {
+        property: 'og:description',
+        content:  'Encontre a música que você deseja cantar',
+      },
+      {
+        property: 'og:image',
+        content: 'https://img.freepik.com/vetores-gratis/fundo-gradiente-de-karaoke_23-2150970025.jpg?semt=ais_hybrid',
+      },
+      {
+        property: 'og:url',
+        content: 'https://busca-musicas-karaoke.vercel.app/',
+      }
+    ]
   }
 };
 </script>
-
-
 
 <style scoped>
 
--- src/main.js
+++ src/main.js
@@ -4,3 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+app.use(metaManager);
+app.mount('#app');
