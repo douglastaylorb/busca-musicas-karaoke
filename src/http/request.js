daea2a9fce81382e02ae2791fda7b90ed2a553ea
douglastaylorb <douglastaylor20@gmail.com>
2024-11-09 11:46:21 -0300

    json local

--- src/http/request.js
+++ src/http/request.js
@@ -1,5 +1,5 @@
 export async function getMusicList() {
-    const response = await fetch('https://gist.githubusercontent.com/douglastaylorb/88529797db016a39883c80e19b1307da/raw/b9e6e1f047d0c09c97123ec92a693c8e3965ded6/gistfile1.txt');
+    const response = await fetch('/musicas.json');
     const data = await response.json();
     
     return data;
